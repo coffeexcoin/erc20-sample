9a06c3af79d06e6e33ed761cd1f6e4a94657c50d
Coffee☕️ <coffee@coffeebot.dev>
2024-11-07 15:31:37 -0500

    minimal erc20

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {ERC20} from "solady-0.0.250/src/tokens/ERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Counter is ERC20 {
+    constructor() {
+        _mint(msg.sender, 10_000_000 * 10 ** decimals());
     }
 
-    function increment() public {
-        number++;
+    function name() public pure override returns (string memory) {
+        return "Counter";
+    }
+
+    function symbol() public pure override returns (string memory) {
+        return "CTR";
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
     }
 }
